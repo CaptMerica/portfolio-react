8ce49b492085856672dc9bae226cf02657fe8d82
CaptMerica <rmerica@live.com>
2023-02-01 19:19:51 -0500

    create nev bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,45 @@
 import { Link } from "react-router-dom";
 
 function NavBar() {
-  return (
-      <nav>
-          <Link 
-              to='/'
-          >
-              Your Name
-          </Link>
+return (
+    <nav>
+        <Link 
+            to='/'
+        >
+            Your Name
+        </Link>
 
-          <ul>
-              <li>
-                  <Link
-                      to='/about'
-                  >
-                      About
-                  </Link>
-              </li>
+        <ul>
+            <li>
+                <Link
+                    to='/about'
+                >
+                    About
+                </Link>
+            </li>
 
-              <li>
-                  <Link
-                      to='/contact'
-                  >
-                      Contact
-                  </Link>
-              </li>
-          </ul>
-      </nav>
-  )
+            <li>
+                <Link
+                    to='/contact'
+                >
+                    Contact
+                </Link>
+            </li>
+            <li>
+                <Link
+                    to='/projects'
+                >
+                    Projects
+                </Link>
+            </li>
+            <li>
+                <Link
+                    to='/Resume'
+                >
+                    Resume
+                </Link>
+            </li>
+        </ul>
+    </nav>
+)
 }
