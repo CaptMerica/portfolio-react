f93bc74eb6d9f91eb614335677ea32233058f118
CaptMerica <rmerica@live.com>
2023-02-01 23:48:20 -0500

    update projectslist

--- src/Pages/ProjectDetails.js
+++ src/Pages/ProjectDetails.js
@@ -3,17 +3,13 @@ import projectsData from "../data/projects"
 
 
 const ProjectDetails() {
-  const { ProjectDetails } = useParams()
+  const { projectDetails } = useParams()
   const projectData = findProject(projectDetails)
   return (
     <>
-      <h1>
-        {projectData.title}
-      </h1>
-      <p>
-        {projectData.description}
-      </p>
-      <img src={projectData.image} alt={projectData.title} />
+      <h1>{projectData[0].title}</h1>
+      <p>{projectData[0].description}</p>
+      <img src={projectData[0].image} alt={projectData.title} />
       <a href={projectData.repositoryLink}><button>GitHub Repo</button></a>
       <a href={projectData.deploymentLink}><button>Test the App!</button></a>
     </>
